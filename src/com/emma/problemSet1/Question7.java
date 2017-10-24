188f4bbb560b40021757874da3681f1d99707952
t00185959 <emmag321@gmail.com>
2017-10-24 15:53:10 +0100

    complete

---
+++ src/com/emma/problemSet1/Question7.java
@@ -0,0 +1,42 @@
+package com.emma.problemSet1;
+
+import jdk.nashorn.internal.scripts.JO;
+
+import javax.swing.*;
+
+public class Question7 {
+    public static void main(String[] args) {
+
+        String numAsString;
+        int num;
+
+        do
+        {
+            numAsString = JOptionPane.showMessageDialog(null,"Please enter a number: ");
+            num = Integer.parseInt(numAsString);
+
+            if(isEven(num)== true)
+            {
+                JOptionPane.showMessageDialog(null,num + "is even", "Even",JOptionPane.PLAIN_MESSAGE);
+            }
+            else
+                JOptionPane.showMessageDialog(null,num + "is odd","Even",JOptionPane.PLAIN_MESSAGE);
+        }while(num!=-1);
+
+    }
+
+
+public static  boolean isEven(int num)
+{
+    boolean even;
+
+    if(num%2 == 0)
+    {
+        even = true;
+    }
+    else
+        even = false;
+
+    return false;
+}
+}

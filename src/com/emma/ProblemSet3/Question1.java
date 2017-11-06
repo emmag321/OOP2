77131488ee69f165bcdb2506e0288aab026c4e5d
Emma <emmag321@gmail.com>
2017-11-06 18:39:09 +0000

    complete 06/11/2017

---
+++ src/com/emma/ProblemSet3/Question1.java
@@ -0,0 +1,17 @@
+package com.emma.ProblemSet3;
+
+public class Question1 {
+
+
+        //attributes
+         private String address;
+         private String type;
+         private float price;
+         private Person owner;
+
+
+
+
+
+    }
+}
